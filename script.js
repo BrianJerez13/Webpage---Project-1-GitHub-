39da07eb328075a2e2057fd0a2cfde8b3bfabf48
BrianJerez1 <73765127+BrianJerez13@users.noreply.github.com>
2021-11-12 03:54:50 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+/*var sliderIndex = 1;
+function showDivs(n){
+  var i;
+  var x = document.getElementsByClassName("slideImg");
+  if(n<x.length){
+    sliderIndex = x.length;
+  }
+  if(n>x.length){
+    sliderIndex = 1;
+  }
+  for(i=0; i<x.length; i++){
+    x[i].style.visibility = "hidden";
+  }
+  x[sliderIndex-1].style.display = "block";
+  document.getElementById("demo").innerHTML = 5+6;
+}
+var sliderIndex = 1;
+showDivs(sliderIndex);
+
+function plusDivs(n){
+  showDivs(sliderIndex += n);
+}*/
